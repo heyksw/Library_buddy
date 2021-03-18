3e2b20728d989095686261165aa4fcaace62f057
heyksw <heyksw0208@naver.com>
2021-03-18 21:18:59 +0900

    first commit test

---
+++ app/src/main/java/com/inha/longstone/TestActivity.java
@@ -0,0 +1,15 @@
+package com.inha.longstone;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test);
+
+    }
+}
