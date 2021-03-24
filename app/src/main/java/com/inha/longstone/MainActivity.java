72bef4ad77b05ff2797ed3d575a0689033b314ca
Goldor <75249093+wya002@users.noreply.github.com>
2021-03-24 15:22:27 +0900

    Update MainActivity.java

--- app/src/main/java/com/inha/longstone/MainActivity.java
+++ app/src/main/java/com/inha/longstone/MainActivity.java
@@ -7,6 +7,9 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -21,8 +24,46 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Intent intent = new Intent(getApplicationContext(), TestActivity.class);
                 startActivity(intent);
+
+                URLConnector url = new URLConnector();
+                url.start();
+                try {
+                    url.join();
+                }
+                catch(Exception e){
+                    e.printStackTrace();
+                }
+
+                String result = url.getTemp();
+
+                System.out.println(ParseJSON(result));
+                ParseJSON(result);
             }
         });
+    }
+    public String ParseJSON(String target){
 
+        try {
+            JSONObject json = new JSONObject(target);
+
+            JSONArray arr = json.getJSONArray("seat");
+
+            for(int i = 0; i < arr.length(); i++){
+                JSONObject json2 = arr.getJSONObject(i);
+
+                System.out.println(json2.getString("s_num"));
+                System.out.println(json2.getString("s_state"));
+                System.out.println(json2.getString("s_name"));
+                System.out.println(json2.getString("user_num"));
+            }
+
+            return "";
+        }
+
+        catch(Exception e){
+            e.printStackTrace();
+        }
+
+        return null;
     }
 }
