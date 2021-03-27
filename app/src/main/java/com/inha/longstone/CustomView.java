21fa8dd7c858d60d0f284f63e024e89046476f1c
heyksw <heyksw0208@naver.com>
2021-03-27 18:31:04 +0900

    kim sang woo : adding design detail

---
+++ app/src/main/java/com/inha/longstone/CustomView.java
@@ -0,0 +1,35 @@
+package com.inha.longstone;
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+public class CustomView extends ConstraintLayout {
+
+    public CustomView(Context context) {
+        super(context);
+        init(context);
+    }
+
+    public CustomView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(context);
+    }
+
+    public CustomView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(context);
+    }
+
+    public CustomView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        init(context);
+    }
+
+    private void init(Context context) {
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        inflater.inflate(R.layout.table, this);
+
+    }
+}
--- app/src/main/java/com/inha/longstone/TestActivity.java
+++ app/src/main/java/com/inha/longstone/TestActivity.java
@@ -3,6 +3,7 @@ package com.inha.longstone;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class TestActivity extends AppCompatActivity {
 
@@ -11,5 +12,10 @@ public class TestActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
 
+        // 버튼 비활성화 코드
+        Button btn =(Button)findViewById(R.id.btn1);
+        btn.setEnabled(false);
+
+        final CustomView indicator2 = (CustomView) findViewById(R.id.custom);
     }
 }
