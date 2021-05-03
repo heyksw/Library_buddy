19581894a9436a641d4cefee2177538270259f06
heyksw <heyksw0208@naver.com>
2021-05-03 20:28:40 +0900

    구글맵api

--- app/src/main/java/com/inha/longstone/MainActivity.java
+++ app/src/main/java/com/inha/longstone/MainActivity.java
@@ -2,68 +2,48 @@ package com.inha.longstone;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
-import org.json.JSONArray;
-import org.json.JSONObject;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Button imageButton = (Button) findViewById(R.id.btn1);
-        imageButton.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), TestActivity.class);
-                startActivity(intent);
-
-                URLConnector url = new URLConnector();
-                url.start();
-                try {
-                    url.join();
-                }
-                catch(Exception e){
-                    e.printStackTrace();
-                }
-
-                String result = url.getTemp();
-
-                System.out.println(ParseJSON(result));
-                ParseJSON(result);
-            }
-        });
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
     }
-    public String ParseJSON(String target){
 
-        try {
-            JSONObject json = new JSONObject(target);
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     *
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
 
-            JSONArray arr = json.getJSONArray("seat");
-
-            for(int i = 0; i < arr.length(); i++){
-                JSONObject json2 = arr.getJSONObject(i);
-
-                System.out.println(json2.getString("s_num"));
-                System.out.println(json2.getString("s_state"));
-                System.out.println(json2.getString("s_name"));
-                System.out.println(json2.getString("user_num"));
-            }
-
-            return "";
-        }
-
-        catch(Exception e){
-            e.printStackTrace();
-        }
-
-        return null;
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions()
+                .position(sydney)
+                .title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 }
