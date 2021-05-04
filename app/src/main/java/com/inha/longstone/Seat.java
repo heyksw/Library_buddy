7df1b77cd306532c65ce42d364dc46f7ffaa042b
Goldor <75249093+wya002@users.noreply.github.com>
2021-05-04 17:43:13 +0900

    Add files via upload

---
+++ app/src/main/java/com/inha/longstone/Seat.java
@@ -0,0 +1,54 @@
+package com.inha.longstone;
+
+public class Seat {
+
+    String s_num;
+    String s_state;
+    String s_name;
+    String user_num;
+    String s_total;
+
+
+    public String getS_total() {
+        return s_total;
+    }
+
+    public void setS_total(String s_total) {
+        this.s_total = s_total;
+    }
+
+    public String getS_state() {
+        return s_state;
+    }
+
+    public String getUser_num() {
+        return user_num;
+    }
+
+    public void setUser_num(String user_num) {
+        this.user_num = user_num;
+    }
+
+    public String getS_name() {
+        return s_name;
+    }
+
+    public void setS_name(String s_name) {
+        this.s_name = s_name;
+    }
+
+    public void setS_state(String s_state) {
+        this.s_state = s_state;
+    }
+
+    public String getS_num() {
+        return s_num;
+    }
+
+    public void setS_num(String s_num) {
+        this.s_num = s_num;
+    }
+
+
+}
+
