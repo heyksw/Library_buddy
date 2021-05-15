9f51e39fc97143b6f19b0b26907a88ae906f1f6b
KimSangWoo <61315014+heyksw@users.noreply.github.com>
2021-05-15 23:34:52 +0900

    Update LabActivity.java

--- app/src/main/java/com/inha/longstone/LabActivity.java
+++ app/src/main/java/com/inha/longstone/LabActivity.java
@@ -7,22 +7,74 @@ import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 import android.view.MenuItem;
-import android.widget.Button;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class LabActivity extends AppCompatActivity {
+    private BottomNavigationView bottomNavigationView;  //바텀 네비게이션 뷰
+    private FragmentManager fm;
+    private FragmentTransaction ft;
+    private Frag_chart frag_chart;
+    private Frag_home frag_home;
+    private Frag_mypage frag_mypage;
+    private Frag_lab frag_lab;
+    android.app.FragmentManager manager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lab);
 
-        // bar button 길이 조정 함수
-        Button bar_button = findViewById(R.id.bar_button);
-        // bar_button.getLayoutParams().width = 100;
+        // 버튼 비활성화 코드
+        //Button btn =(Button)findViewById(R.id.btn1);
+        //btn.setEnabled(false);
+        final CustomView indicator2 = (CustomView) findViewById(R.id.custom1);
 
+        bottomNavigationView = findViewById(R.id.navi);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch(item.getItemId()){
+                    case R.id.chart:
+                        setFrag(0);
+                        break;
+                    case R.id.home:
+                        setFrag(1);
+                        break;
+                    case R.id.mypage:
+                        setFrag(2);
+                        break;
 
+                }
+                return false;
+            }
+        });
+        frag_chart = new Frag_chart();
+        frag_home = new Frag_home();
+        frag_mypage = new Frag_mypage();
+        frag_lab = new Frag_lab();
+
+        setFrag(1);  // 첫 화면은 home
     }
 
+    private void setFrag(int n){
+        fm = getSupportFragmentManager();
+        ft = fm.beginTransaction();
+        switch(n){
+            case 0:
+                ft.replace(R.id.main_frame,frag_chart);
+                ft.commit();
+                break;
+            case 1:
+                ft.replace(R.id.main_frame,frag_lab);
+                ft.commit();
+                break;
+            case 2:
+                ft.replace(R.id.main_frame,frag_mypage);
+                ft.commit();
+                break;
+
+        }
+
+    }
 }
