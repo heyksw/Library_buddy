f295b55e180a48ce9b41ef6c8dfcf3758b4cd6e5
KimSangWoo <61315014+heyksw@users.noreply.github.com>
2021-05-17 02:15:50 +0900

    Update IncheonList.java

--- app/src/main/java/com/inha/longstone/IncheonList.java
+++ app/src/main/java/com/inha/longstone/IncheonList.java
@@ -15,6 +15,24 @@ public class IncheonList extends AppCompatActivity {
         setContentView(R.layout.activity_incheon_list);
 
         Button Inha = findViewById(R.id.Inha);
+        Button univ1 = findViewById(R.id.univ1);
+        Button univ2 = findViewById(R.id.univ2);
+        Button univ3 = findViewById(R.id.univ3);
+        Button univ4 = findViewById(R.id.univ4);
+        Button univ5 = findViewById(R.id.univ5);
+        Button univ6 = findViewById(R.id.univ6);
+        Button univ7 = findViewById(R.id.univ7);
+
+        univ1.setBackgroundDrawable(null);
+        univ2.setBackgroundDrawable(null);
+        univ3.setBackgroundDrawable(null);
+        univ4.setBackgroundDrawable(null);
+        univ5.setBackgroundDrawable(null);
+        univ6.setBackgroundDrawable(null);
+        univ7.setBackgroundDrawable(null);
+        Inha.setBackgroundDrawable(null);
+
+
         Inha.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
