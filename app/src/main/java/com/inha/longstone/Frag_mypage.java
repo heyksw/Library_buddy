357f20255fe69c9906a56d2ad06d492e503a3e03
KimSangWoo <61315014+heyksw@users.noreply.github.com>
2021-05-17 01:34:44 +0900

    Update Frag_mypage.java

--- app/src/main/java/com/inha/longstone/Frag_mypage.java
+++ app/src/main/java/com/inha/longstone/Frag_mypage.java
@@ -1,9 +1,12 @@
 package com.inha.longstone;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -16,6 +19,21 @@ public class Frag_mypage extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.frag_mypage,container,false);
+
+        Button swap = view.findViewById(R.id.swap);
+        swap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://192.168.0.13:8090"));
+                startActivity(intent);
+
+            }
+        });
+
+
+
         return view;
+
+
     }
 }
