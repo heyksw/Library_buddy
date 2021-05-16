50d28c35215e649b91d23258be849b4821b61d47
KimSangWoo <61315014+heyksw@users.noreply.github.com>
2021-05-17 02:16:11 +0900

    Update InhaList.java

--- app/src/main/java/com/inha/longstone/InhaList.java
+++ app/src/main/java/com/inha/longstone/InhaList.java
@@ -14,7 +14,42 @@ public class InhaList extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inha_list);
 
+        // 정석
+        Button button1 = findViewById(R.id.button1);
+        button1.setBackgroundDrawable(null);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(getApplicationContext(), Test1.class);
+                startActivity(intent);
+            }
+        });
+
+        // 60주년 월천
+        Button button2 = findViewById(R.id.button2);
+        button2.setBackgroundDrawable(null);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(getApplicationContext(), Test2.class);
+                startActivity(intent);
+            }
+        });
+
+        // 하이테크 해동
+        Button button3 = findViewById(R.id.button3);
+        button3.setBackgroundDrawable(null);
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(getApplicationContext(), Test3.class);
+                startActivity(intent);
+            }
+        });
+
+
         Button button4 = findViewById(R.id.button4);
+        button4.setBackgroundDrawable(null);
         button4.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
