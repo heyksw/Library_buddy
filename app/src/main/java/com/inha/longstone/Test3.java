85aa3f55d54c77050997d3cb8df8ae4f9eb6d621
KimSangWoo <61315014+heyksw@users.noreply.github.com>
2021-06-16 16:42:43 +0900

    Update Test3.java

--- app/src/main/java/com/inha/longstone/Test3.java
+++ app/src/main/java/com/inha/longstone/Test3.java
@@ -3,6 +3,9 @@ package com.inha.longstone;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 
 public class Test3 extends AppCompatActivity {
 
@@ -10,5 +13,12 @@ public class Test3 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test3);
+
+        View redBar = findViewById(R.id.redBar3);
+        View blueBar = findViewById(R.id.blueBar3);
+
+        Animation animation = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.scale);
+        redBar.startAnimation(animation);
+        blueBar.startAnimation(animation);
     }
 }
