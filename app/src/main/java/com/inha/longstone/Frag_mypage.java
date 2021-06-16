d86389c1474cfa98c0a3be7c79fe902162f2872b
KimSangWoo <61315014+heyksw@users.noreply.github.com>
2021-06-16 17:22:23 +0900

    Update Frag_mypage.java

--- app/src/main/java/com/inha/longstone/Frag_mypage.java
+++ app/src/main/java/com/inha/longstone/Frag_mypage.java
@@ -57,12 +57,13 @@ public class Frag_mypage extends Fragment {
                             use_time.setText("02:34:0"+timer_sec);
                         }
                         else{
+                            
+                            use_time.setText("02:34:"+timer_sec);
                             if(timer_sec==59){
                                 timer_sec=0;
                             }
-                            use_time.setText("02:34:"+timer_sec);
                         }
-                        
+
                     }
                 };
 
